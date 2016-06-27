d1b04ab5171a2f608de9f04780c9d3072e80971a
Dillon Huff <dillonhuff@gmail.com>
2016-06-26 19:40:54 -0700

    Added several new pages and an about blurb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  post '/new_problem', to: 'static_pages#submit_answer'
+  get '/home' => 'static_pages#home'
+  get '/about' =>'static_pages#about'
+  get '/pundits' => 'static_pages#pundits'
+  get '/events' => 'static_pages#events'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
