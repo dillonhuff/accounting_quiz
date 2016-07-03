bdf3546273e276ba17657c04321d24bcc954b8d6
Dillon Huff <dillonhuff@gmail.com>
2016-07-02 19:27:07 -0700

    Removed edit and destroy options from reviews index. Also improved review display

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :prediction
+  validates :prediction_id, presence: true
+  validates :rating, presence: true, :inclusion => 0..100
+  validates :content, presence: true, length: { maximum:10000 }
 end
