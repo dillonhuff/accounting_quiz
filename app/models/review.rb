fd0696b5b84332440e73a71cf5df83de9a04aeec
Dillon Huff <dillonhuff@gmail.com>
2016-07-03 15:57:21 -0700

    Now have proper title inclusion in new reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
   validates :prediction_id, presence: true
   validates :rating, presence: true, :inclusion => 0..100
   validates :content, presence: true, length: { maximum:10000 }
+  validates :title, presence: true, length: { minimum:1, maximum:50 }
 end
